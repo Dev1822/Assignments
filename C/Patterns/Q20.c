87d9c2a56cdb06995773462fcc500eb4f4786661
Dev1822 <dev.d.patel.cg@gmail.com>
2025-09-15 11:16:07 +0530

    Pattern Programs

--- C/Patterns/Q20.c
+++ C/Patterns/Q20.c
@@ -2,13 +2,14 @@
 
 int main() {
     int n;
-    scanf("%d", &n);
-    int x;
+    scanf("%d",&n);
+    int x=64;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=(n-i)+1;j++){
-            x=64+j;
-            printf("%c",x);
+        for(int j=1;j<=i;j++){
+            x++;
+            printf("%c \t",x);
         }
+        
         printf("\n");
     }
 
